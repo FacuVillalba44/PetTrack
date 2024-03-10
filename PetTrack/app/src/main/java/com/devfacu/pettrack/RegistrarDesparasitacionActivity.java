d962f019efb0f77defab4999da7db1e4dd91a607
Facundo Villalba <vfacu44@gmail.com>
2024-03-10 03:52:35 -0300

    añadiendo logica a desparasitacion

--- PetTrack/app/src/main/java/com/devfacu/pettrack/RegistrarDesparasitacionActivity.java
+++ PetTrack/app/src/main/java/com/devfacu/pettrack/RegistrarDesparasitacionActivity.java
@@ -1,6 +1,11 @@
 package com.devfacu.pettrack;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +14,24 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class RegistrarDesparasitacionActivity extends AppCompatActivity {
+    private static final String TAG = "activity_registrar_desparasitacion";
+    private DatePickerDialog.OnDateSetListener fechaAdministrada, siguienteAdministracion;
+    EditText marcaDesparasitante;
+    Spinner tipoDesparasitante;
+    TextView tvfechAdministracion,tvfechProximaAdm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_registrar_desparasitacion);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        marcaDesparasitante = findViewById(R.id.etMarcaDesparasitante);
+        tipoDesparasitante = findViewById(R.id.spinnerTipoDesparasitante);
+        String []tiposDesparasitante={"Comprimido","Gota","Inyectable"};
+        ArrayAdapter<String> tiposadapter= new ArrayAdapter<>(RegistrarDesparasitacionActivity.this,R.layout.spinner_custom_commons,tiposDesparasitante);
+        tipoDesparasitante.setAdapter(tiposadapter);
+        tvfechAdministracion=findViewById(R.id.tvFechaAdministrado);
+        tvfechProximaAdm=findViewById(R.id.tvFechaAdministrado2);
+
+
     }
 }
