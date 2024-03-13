332a457a255bd0cae4090b19f9285b78131b0522
Facundo Villalba <vfacu44@gmail.com>
2024-03-12 22:51:07 -0300

    feature cardlist mascotas

---
+++ PetTrack/app/src/main/java/com/devfacu/pettrack/TusMascotasActivity.java
@@ -0,0 +1,15 @@
+package com.devfacu.pettrack;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TusMascotasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tus_mascotas);
+
+    }
+}
