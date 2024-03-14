211d154b435d7d4faa7433768d51abbace9b3b9b
Facundo Villalba <vfacu44@gmail.com>
2024-03-14 18:03:30 -0300

    corrigiendo dependencias

--- PetTrack/app/src/main/java/com/devfacu/pettrack/TusMascotasActivity.java
+++ PetTrack/app/src/main/java/com/devfacu/pettrack/TusMascotasActivity.java
@@ -2,8 +2,7 @@ package com.devfacu.pettrack;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,6 +20,7 @@ public class TusMascotasActivity extends AppCompatActivity {
     private static final int TU_CODIGO_DE_EDICION = 1;
     private ArrayList<Mascota> listaArrayMascotas;
     private ListaMascotasAdapter listaMascotasAdapter;
+    ImageButton volverAHome_TM;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
